5b474b1c1bbcdb411cbccd144161ff7a7801cee0
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-08-29 14:53:21 +0700

    init project

---
+++ server/server.mjs
@@ -0,0 +1,14 @@
+import express from 'express'
+import http from "http"
+import { ApolloServer } from "@apollo/server";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+})
+
+await server.start();
